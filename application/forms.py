56e39da4325c2c197f2f6ecb08d9dbf026a3448f
Hannah Lister-Sims <hlistersims@gmail.com>
2020-11-10 19:01:11 +0000

    updated project

---
+++ application/forms.py
@@ -0,0 +1,23 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, SelectField
+from wtforms.validators import DataRequired, ValidationError
+
+from application.models import Ingredients, Method, Recipe
+
+class IngredientsForm( FlaskForm ) :
+    name = StringField( 'Ingredient',
+        validators = [
+            DataRequired()
+        ]
+    )
+    submit = SubmitField( 'Add ingredient' )
+
+class MethodForm( FlaskForm ) :
+    steps = TextAreaField( 'Method',
+        validators = [
+            DataRequired()
+        ]
+    )
+    time = TimeField( 'Estimated time', format='%H:%M' )
+    submit = SubmitField( 'Add Method' )
+
